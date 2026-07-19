89be9a469fd9b7686347985b7f161d1fa5a0facc
robeshell <robeshell@users.noreply.github.com>
2026-07-19 04:52:00 +0000

    deploy: publish website and web app [skip ci] 50f1b21b7e2d0af652218b852f7422a584294f76

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2743030878" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "77724164" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
